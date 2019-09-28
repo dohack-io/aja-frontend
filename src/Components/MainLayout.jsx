69c5f88821c8a43b2e5dacd71d6937b4af052afe
pl0erre <luca.angona@web.de>
2019-09-28 11:06:18 +0200

    added wrapper

--- src/Components/MainLayout.jsx
+++ src/Components/MainLayout.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './MainLayout.css';
+import {Nav} from './Nav';
+import {Wrapper} from './Wrapper';
+import {Footer} from './Footer';
+
 
 export default class MainLayout extends Component {
   render() {
     return (
       <div className="mainlayout">
-        
+        <Nav />
+          <Wrapper>
+            {this.props.children}
+          </Wrapper>
+        <Footer />
       </div>
     )
   }
