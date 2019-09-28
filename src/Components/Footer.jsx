e677d30b9ecd77038ad448bbf5ad46a3282ff206
pl0erre <luca.angona@web.de>
2019-09-28 13:27:20 +0200

    worked on footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,6 +5,21 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="footer">
+        <div className="footer__box">
+          <p>bla</p>
+          <p>bla</p>
+          <p>bla</p>
+        </div>
+        <div className="footer__box">
+          <p>blubb</p>
+          <p>blubb</p>
+          <p>blubb</p>
+        </div>
+        <div className="footer__box">
+          <p>penis</p>
+          <p>penis</p>
+          <p>penis</p>
+        </div>
       </div>
     )
   }
