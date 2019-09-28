5e984a923b3db46a81b77389654a0b26f7bbb68e
pl0erre <luca.angona@web.de>
2019-09-28 12:23:36 +0200

    changes

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,14 +3,30 @@ import './Nav.css';
 import {Link} from 'react-router-dom';
 
 export default class Nav extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null
+    }
+  }
+
+  toggleBurger() {
+    document.getElementsByClassName("navbar__burgerBox")[0].style.display = none;
+    
+  }
+
+
+
+
   render() {
     return (
       <div className="navbar">
         <Link to="/" className="navbar__link"><i class="fab fa-pagelines fa-2x"></i></Link>
         <div>
           <i class="fas fa-bars fa-2x"></i>
-          
-          <div className="navbar__linkContainer">
+
+          <div className="navbar__burgerBox">
             <Link to="/login" className="navbar__link">Login</Link>
             <Link to="/signup" className="navbar__link">SignUp</Link>
             <Link to="/about" className="navbar__link">About</Link>
