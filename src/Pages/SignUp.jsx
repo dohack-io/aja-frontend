5072bc1210e338a05c3df83b112637f557596381
Jennifer V Phan <jennyphan181295@gmail.com>
2019-09-28 13:24:37 +0200

    sign up auth is working now!

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './SignUp.css';
 import {Link} from "react-router-dom";
 import MainLayout from "../Components/MainLayout";
+import AuthService from "../Components/AuthService";
 
 export default class SignUp extends Component {
   constructor(props){
@@ -12,8 +13,30 @@ export default class SignUp extends Component {
                     email:"",
                     password: "",
                     confirmedPassword:""};
+    this.service= new AuthService();
   }  
   
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    const name = this.state.name;
+    const surname = this.state.surname;
+    const email = this.state.email;
+    const password=this.state.password;
+    const confirmedPassword= this.state.confirmedPassword;
+    this.service.signup(name,surname,email,password, confirmedPassword)
+    .then( response => {
+      console.log(response)
+        this.setState({ name: "",
+                        surname:"",
+                        email:"",
+                        password: "",
+                        confirmedPassword:""
+                      });
+        this.props.history.push('/user/profile')
+    })
+    .catch( error => console.log(error) )
+  }
+
   handleChange = (event) => {  
     const {name, value} = event.target;
     this.setState({[name]: value});
