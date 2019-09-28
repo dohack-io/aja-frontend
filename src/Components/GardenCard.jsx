cd43b603fc1b58ecd1f0fa1415add1964e68dbe7
pl0erre <luca.angona@web.de>
2019-09-28 18:01:47 +0200

    garden collection

--- src/Components/GardenCard.jsx
+++ src/Components/GardenCard.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
 import "./GardenCard.css";
 
 export default class GardenCard extends Component {
     render() {
         return (
-            <div className="gardenCard">
-                <div className="gardenCard__image"></div>
-                <h3 className="gardenCard__name">Garden-name</h3>
-            </div>
+            <>
+                <div className="gardenCard__image">
+                    <Link to={{ pathname: "/garden/" + this.props.garden.id,
+                                state: {garden: this.props.garden}}}  className="gardenCard__link" garden={this.props.garden}><h3 className="gardenCard__name">{this.props.garden.team.name}</h3></Link>   
+                </div>
+                
+            </>
         )
     }
 }
