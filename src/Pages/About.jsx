5ae1eace40fc992c643971a36dbdfe10d5120924
pl0erre <luca.angona@web.de>
2019-09-28 11:54:35 +0200

    nav styling, about page

---
+++ src/Pages/About.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./About.css";
+import MainLayout from "../Components/MainLayout";
+
+export default class About extends Component {
+  render() {
+    return (
+      <MainLayout>
+        <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+      </MainLayout>
+
+    )
+  }
+}
