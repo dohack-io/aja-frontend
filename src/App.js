0bcc062e428ebf8455c30bf20499310aa66e2013
Jennifer V Phan <jennyphan181295@gmail.com>
2019-09-28 11:30:02 +0200

    sign up page setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route} from 'react-router-dom';
 import './App.css';
 import Home from "./Pages/Home";
 import Profile from "./Pages/Profile";
+import SignUp from "./Pages/SignUp";
 
 class App extends Component {
   constructor(props){
@@ -13,7 +14,9 @@ class App extends Component {
     return(
       <>
         <Route exact path="/" render = {(routeProps)=> <Home/>}/>
+        <Route exact path="/signup" render ={()=><SignUp/>}/>
         <Route exact path="/user/profile" render = {(routeProps)=> <Profile/>}/>
+
       </>
     )
   }
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -4,11 +4,49 @@ import {Link} from "react-router-dom";
 import MainLayout from "../Components/MainLayout";
 
 export default class SignUp extends Component {
+  constructor(props){
+    super(props);
+    this.formRef = React.createRef();
+    this.state = {  name: "",
+                    surname:"",
+                    email:"",
+                    password: "",
+                    confirmedPassword:""};
+  }  
+  
+  handleChange = (event) => {  
+    const {name, value} = event.target;
+    this.setState({[name]: value});
+  }
+
   render() {
     return (
       <MainLayout>
         <div className="signupForm">
           <h3>Sign up</h3>
+          <form className="signupForm__input" onSubmit={this.handleFormSubmit}>
+
+            <label>Name:</label>
+            <input type="text" name="name" value={this.state.name} onChange={ e => this.handleChange(e)}/>
+            
+            <label>Surname:</label>
+            <input type="text" name="surname" value={this.state.surname} onChange={ e => this.handleChange(e)}/>
+            
+            <label>Email:</label>
+            <input type="text" name="email" value={this.state.email} onChange={ e => this.handleChange(e)}/>
+            
+            <label>Password:</label>
+            <input type="password" name="password" value={this.state.password} onChange={ e => this.handleChange(e)} />
+            
+            <label>Confirm Password:</label>
+            <input type="password" name="confirmedPassword" value={this.state.confirmedPassword} onChange={ e => this.handleChange(e)} />
+            
+            <button className="submitBtn" type="submit">Sign up</button>
+        </form>
+
+        <p>Already have an account? 
+            <Link to={"/login"} style={{ color: 'rgb(5, 5, 5)', textDecoration:"underline" }}> Login</Link>
+        </p>
         </div>
       </MainLayout>
     )
