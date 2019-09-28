18412cce1c79b948a844ceae4158c07d6ca90b7f
pl0erre <luca.angona@web.de>
2019-09-28 12:13:34 +0200

    started burger

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -8,9 +8,14 @@ export default class Nav extends Component {
       <div className="navbar">
         <Link to="/" className="navbar__link"><i class="fab fa-pagelines fa-2x"></i></Link>
         <div>
-          <Link to="/login" className="navbar__link">Login</Link>
-          <Link to="/signup" className="navbar__link">SignUp</Link>
-          <Link to="/about" className="navbar__link">About</Link>
+          <i class="fas fa-bars fa-2x"></i>
+          
+          <div className="navbar__linkContainer">
+            <Link to="/login" className="navbar__link">Login</Link>
+            <Link to="/signup" className="navbar__link">SignUp</Link>
+            <Link to="/about" className="navbar__link">About</Link>
+          </div>
+          
         </div>
       </div>
     )
