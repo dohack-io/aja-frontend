7b63ce16bc42b32c57877f7e928c543c2d866c23
Niklas Eicker <git@nikl.me>
2019-09-29 05:15:18 +0200

    some css updates

--- src/Components/MainLayout.jsx
+++ src/Components/MainLayout.jsx
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
-import './MainLayout.css';
-import Nav from './Nav';
-import Wrapper from './Wrapper';
-import Footer from './Footer';
-
+import React, { Component } from "react";
+import "./MainLayout.css";
+import Nav from "./Nav";
+import Wrapper from "./Wrapper";
+import Footer from "./Footer";
 
 export default class MainLayout extends Component {
   render() {
     return (
       <div className="mainlayout">
-        <Nav {...this.props}/>
-          <Wrapper>
-            {this.props.children}
-          </Wrapper>
+        <Nav {...this.props} />
+        <Wrapper>{this.props.children}</Wrapper>
         <Footer />
       </div>
-    )
+    );
   }
 }
