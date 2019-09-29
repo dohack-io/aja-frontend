cf28456d0774551275b30b29143fb85a1ae858d2
pl0erre <luca.angona@web.de>
2019-09-29 09:00:41 +0200

    working on not-logged in homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -134,23 +134,22 @@ class Home extends Component {
     } else {
       return (
         <MainLayout>
-          <div className="homepage">
-            <div className="homepage__headerImg"></div>
-            <div className="homepage__welcome">
-              <h1>Welcome to Aja's Garden</h1>
-              <p>
-                We make cities greener and bring the neighborhood together. With
-                Aja everyone can take care of a small pice of land and transform
-                a brown and boring parcel into a blooming garden. You can browse
-                open gardening areas on our map after logging in.
-              </p>
-              <Link to="/signup" className="homepage__getStarted">
-                {"Sign up"}
-              </Link>
-              <Link to="/login" className="homepage__getStarted">
-                {"Log in"}
-              </Link>
-            </div>
+            <div className="homepage__headerImg">
+              <div className="homepage__welcome">
+                <h1>Welcome to Aja</h1>
+                <p>
+                  We make cities greener and bring the neighborhood together. With
+                  Aja everyone can take care of a small pice of land and transform
+                  a brown and boring parcel into a blooming garden. You can browse
+                  open gardening areas on our map after logging in.
+                </p>
+                <Link to="/signup" className="homepage__getStarted">
+                  {"Sign up"}
+                </Link>
+                <Link to="/login" className="homepage__getStarted">
+                  {"Log in"}
+                </Link>
+              </div> 
           </div>
         </MainLayout>
       );
